53b22572f3af85a90825dab041a8d96f89878893
Calbusto <calbustohypnose@gmail.com>
2025-01-31 23:19:00 +0000

    ajout de flask et autre

--- cogs/utilitaires.py
+++ cogs/utilitaires.py
@@ -29,6 +29,22 @@ class Utilitaire(commands.Cog):
         else:
             await ctx.send("Le salon d'annonce n'a pas été trouvé.")
 
+# EFFACER COMMAND -----------------------------
+
+    @discord.app_commands.hybrid_command(name="effacer", description="Efface un nombre spécifié de messages")
+    @commands.has_permissions(manage_messages=True)
+    async def effacer(self, ctx, nombre: int):
+        """Efface un nombre spécifié de messages"""
+        
+        # Vérifie que le nombre de messages à supprimer est dans la plage valide
+        if nombre < 1 or nombre > 100:
+            await ctx.send("Tu peux supprimer entre 1 et 100 messages à la fois.")
+            return
+
+        # Efface les messages
+        deleted = await ctx.channel.purge(limit=nombre)
+        await ctx.send(f"{len(deleted)} messages ont été supprimés.", delete_after=5)
+
 
 def setup(bot):
     bot.add_cog(Utilitaire(bot))
